b52cefea910ab4f22b1359086972efbccabe1d18
gport7 <gport@vt.edu>
2022-03-08 19:05:19 -0800

    finish 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let output = 0;
+    if (x < 0 || y < 0 || typeof(x) !== "number" || typeof(y) !== "number"){
+        output = "ERROR";
+    } else {
+        if (x < y) {
+            for (let i = x; i <= y; i++) {
+                output += i;
+            }
+        } else {
+            for (let i = y; i <= x; i++) {
+                output += i;
+            }
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
