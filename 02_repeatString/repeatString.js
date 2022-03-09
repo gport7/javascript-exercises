999f4b9e6de36659f966158dd54ffd97b6255621
gport7 <gport@vt.edu>
2022-03-08 18:22:03 -0800

    finish 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+    if (x < 0) {
+        return "ERROR";
+    } else {
+        let output = "";
+        for (let i = 0; i < x; i++) {
+            output += string;
+        }
+        return output;
+    }
 };
 
 // Do not edit below this line
