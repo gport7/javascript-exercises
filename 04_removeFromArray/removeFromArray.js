a89be9b632450f70b1c1905cfe0dedc2722acf7d
gport7 <gport@vt.edu>
2022-03-08 18:52:46 -0800

    finish 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argsArray) {
+    let output = array;
+    for (let i = 0; i < argsArray.length; i++) {
+        let subtractedItem = argsArray[i];
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === subtractedItem) {
+                output.splice(j, 1);
+            }
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
