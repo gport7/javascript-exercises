81372ade757b697adaaab0e8e4b9074be500b501
gport7 <gport@vt.edu>
2022-03-09 18:36:49 -0800

    finish 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(...numArray) {
+  let output = 0;
+  for(const num of numArray) {
+    output += num;
+  }
+	return output;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	let output = x - y;
+  return output;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let output = 0;
+  for(const num of numArray) {
+    output += num;
+  }
+	return output;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let output = 1;
+  for(const num of numArray) {
+    output *= num;
+  }
+	return output;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let ouptut = x ** y;
+	return ouptut;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let output = 1;
+	for (let i = num; i > 0; i--) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
