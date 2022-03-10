26078996445cfa4f4e97617619203e5befd6b865
gport7 <gport@vt.edu>
2022-03-09 18:21:29 -0800

    finish 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * 5 / 9;
+  let cRounded = +c.toFixed(1);
+  return cRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * 9 / 5 + 32;
+  let fRounded = +f.toFixed(1);
+  return fRounded;
 };
 
 // Do not edit below this line
