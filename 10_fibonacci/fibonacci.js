7ad7078077b0002d2a28178cb9495bca5764613f
gport7 <gport@vt.edu>
2022-03-09 20:36:29 -0800

    finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    const num = +x;
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+        let fibArray = [1];
+        for (let i = 0; i < num; i++) {
+            let sumPreceding = 1;
+            if (i > 0) {
+                sumPreceding = fibArray[i - 1] + fibArray [i];
+            }
+            fibArray.push(sumPreceding);
+        }
+        return fibArray[num - 1];
+    }
 };
 
 // Do not edit below this line
