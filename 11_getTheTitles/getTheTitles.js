78ce1123224ab9c21feb2ba41f0b1fd43d31df3c
gport7 <gport@vt.edu>
2022-03-10 19:25:47 -0800

    finish 11, map practice

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(x) {
+    return x.map((a) => a.title);    
 };
+//'a' is representative of the object in the array 'x'
+//'a' can be named anything
+//'title' is a key in the object 'a', so 'a.title' is kind of like title is the index 
 
 // Do not edit below this line
 module.exports = getTheTitles;
